506ece364d42eee08e97a81c51e158492ad3fd5d
Samson Njuguna <sam.44njuguna@gmail.com>
2022-08-15 18:35:33 +0300

    updated components

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-import '../styles/globals.css'
+import { Layout } from '../components';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
